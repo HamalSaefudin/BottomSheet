4e03b1fb11aa9af8b14ede6fe4da4e43db3d4664
Hamal <Hamalsaefudin48@gmail.com>
2022-11-27 11:35:18 +0700

    Initial Commit

---
+++ SecondAssignment/SecondAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondAssignmentApp.swift
+//  SecondAssignment
+//
+//  Created by Hamal Saefudin on 27/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SecondAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
